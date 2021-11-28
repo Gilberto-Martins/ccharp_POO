c1afb67f995556b375aefe4389f3db66beaa4d43
Gilberto Martins <gilbertomartins643@gmail.com>
2021-11-28 14:36:17 -0300

    uppdate Encapsulamento

--- exemploPOO/model/retangulo.cs
+++ exemploPOO/model/retangulo.cs
@@ -1,19 +1,38 @@
+using System;
+
 namespace exemploPOO.model
 {
     public class retangulo
     {
         private double largura;
         private double comprimento;
+        private bool validador;
 
         public void definirValores(double larg, double comp)
         {
-            this.comprimento = comp;
-            this.largura = larg;
+            if (larg > 0 && comp > 0)
+            {
+                validador = true;
+                this.comprimento = comp;
+                this.largura = larg;
+            }
+            else
+            {
+                Console.WriteLine("Não aceita valores negativos!!");
+            }
         }
 
         public double Area()
         {
-            return comprimento * largura;
+            if (validador)
+            {
+                return comprimento * largura;
+            }
+            else
+            {
+                Console.WriteLine("Ensira valores positivos!!");
+                return 0;
+            }
         }
     }
 }
