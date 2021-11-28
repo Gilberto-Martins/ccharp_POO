cfd4c72ffc34e35f60e955e1e7b1b16cebc646c6
Gilberto Martins <gilbertomartins643@gmail.com>
2021-11-28 14:58:37 -0300

    Metodo Construtor

--- exemploPOO/Program.cs
+++ exemploPOO/Program.cs
@@ -7,19 +7,23 @@ namespace exemploPOO
     {
         static void Main(string[] args)
         {
+            
+            Pessoa p1 = new Pessoa("Gilberto", 22);
+
             /*
-            Pessoa p1 = new Pessoa();
             Console.Write("Digite seu nome: ");
             p1.nome = Console.ReadLine();
             Console.Write("Digite sua idade: ");
             p1.idade = Convert.ToInt32(Console.ReadLine());
+            */
 
             Console.WriteLine();
             p1.apresentar();
-            */
+            
 
 
             //encapisulamento
+            /*
             retangulo ret = new retangulo();
             Console.Write("Digite um valor em decimal para largura: ");
             double comp1 = Convert.ToDouble(Console.ReadLine());
@@ -29,6 +33,7 @@ namespace exemploPOO
             ret.definirValores(comp1,larg1);
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine("O valor da area é: " + ret.Area());
+            */
         }
     }
 }
--- exemploPOO/model/Pessoa.cs
+++ exemploPOO/model/Pessoa.cs
@@ -7,6 +7,22 @@ namespace exemploPOO.model
         public string nome  {   get;    set; }
         public int idade {  get;    set;    }
 
+
+
+        //Constutor
+        //defini que quando instanciar uma classe você pode definir seu atributos
+        //pode ter mais de um construtor
+        //se não definir um construtor o programa vai usar um por padrão, que não precissa especificar
+        //os valores dos atributos
+        public Pessoa (string Nome, int Idade)
+        {
+            this.nome = Nome;
+            this.idade = Idade;
+        }
+
+        
+
+        //metodos
         public void apresentar()
         {
             Console.WriteLine("meu nome é "+nome+" e tenho "+idade+" anos.");
